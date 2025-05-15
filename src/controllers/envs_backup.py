82e7d60a80031901ef053faecf2601ba60baccdb
cisbaf <cisbaf@servidorlinux.cisbaf.org.br>
2025-05-15 15:08:49 -0300

    adicionando o scipt load env

--- src/controllers/envs_backup.py
+++ src/controllers/envs_backup.py
@@ -6,8 +6,10 @@ class EnvsBackup(BackupRepository):
 
     def set_up(self):
         self.path_env = "/etc/cisbaf-setup"
+        self.path_load_env = "/usr/local/bin/load-cisbaf-env"
     
     def run(self):
+        shutil.copy2(self.path_load_env, self.path_backup)
         for item in os.listdir(self.path_env):
             s = os.path.join(self.path_env, item)
             d = os.path.join(self.path_backup, item)
